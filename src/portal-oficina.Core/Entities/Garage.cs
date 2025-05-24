2c98fbab374d7303f4d6eec8016a2ba9330b6f67
devigoraugusto <i.augusto@hotmail.com>
2025-05-24 00:28:59 -0300

    add Entities `User` and `Garage`

---
+++ src/portal-oficina.Core/Entities/Garage.cs
@@ -0,0 +1,11 @@
+﻿namespace portal_oficina.Core.Entities
+{
+    public class Garage
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string Services { get; set; }
+
+    }
+}
---
+++ src/portal-oficina.Core/Entities/User.cs
@@ -0,0 +1,10 @@
+﻿namespace portal_oficina.Core.Entities
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        public string PasswordHash { get; set; }
+
+    }
+}
